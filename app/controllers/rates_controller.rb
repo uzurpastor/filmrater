836a9c654edec25a0aad7c2e2d655b88c4194703
uzurpastor <shevchenko.andrey.uzurpastor@gmail.com>
2022-09-03 13:25:46 +0300

    generate: rates controller with create action

---
+++ app/controllers/rates_controller.rb
@@ -0,0 +1,4 @@
+class RatesController < ApplicationController
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'rates/create'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users
 end

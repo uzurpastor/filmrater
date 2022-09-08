c4e71b399dcf46dc76bf4008ca0a8fa2db93617c
uzurpastor <shevchenko.andrey.uzurpastor@gmail.com>
2022-09-08 13:08:37 +0300

    devel: setup authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
+  before_action :current_ability
+
+  def current_ability
+    @current_ability ||= Ability.new(current_user)
+  end
+
+  def safety?
+    SafetyChecker.safety?(current_ability)
+  end
 end

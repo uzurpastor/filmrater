19780a474df0611ca8a4c7affb7c9b01f6144483
uzurpastor <shevchenko.andrey.uzurpastor@gmail.com>
2022-09-10 18:52:28 +0300

    refactor: clean the rate controller with service help

--- app/controllers/rates_controller.rb
+++ app/controllers/rates_controller.rb
@@ -1,14 +1,12 @@
 class RatesController < ApplicationController
   before_action :authenticate_user!
-  before_action :init_options
 
   def create
     @rate = current_user.rates.new(rates_create_params)
-
     @rate.save
 
-    @options.merge!(film: current_film)
-    @options.merge!(rate_params: RateParamsCollector.call(current_film, current_user))
+    @options = OptionSetter.new(current_film, current_user)
+                           .options(%i[rate_params film])
 
     respond_to do |format| 
       format.html {redirect_to film_path(@options[:film])}
@@ -17,10 +15,6 @@ class RatesController < ApplicationController
   end
 
   private
-
-  def init_options
-    @options = {}  
-  end
 
   def current_film
     Film.find(params[:rate][:film_id])
--- app/services/option_setter.rb
+++ app/services/option_setter.rb
@@ -10,14 +10,21 @@ class OptionSetter
     case options
     when Symbol
       add_rate_params if options == :rate_params
+      add_category    if options == :category
+      add_film        if options == :film
     when Array
       add_rate_params if options.include?(:rate_params)        
-      add_category if options.include?(:category)        
+      add_category    if options.include?(:category)        
+      add_film        if options.include?(:film)
     end
-      
+    @options
   end
 
   private
+
+  def add_film
+    @options.merge!(film: @film) 
+  end
 
   def add_rate_params
     @options.merge!(rate_params: collect_rate_params)

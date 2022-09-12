529a294f299eca4b026fe7c3a2192aeebb834394
uzurpastor <shevchenko.andrey.uzurpastor@gmail.com>
2022-09-12 15:23:02 +0300

    devel: remove unusing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   
   resources :films, except: :destroy
-  get 'films/:id/destroy', 
-      to: 'films#destroy', 
-      as: 'destroy_film'
+  get   'films/:id/destroy',  to: 'films#destroy',  as: 'destroy_film'
+  post  'rates',              to: 'rates#create',   as: 'create_rate'
 
-  post 'rates', 
-      to: 'rates#create',
-      as: 'create_rate'
-  devise_for :users
+  devise_for :users, skip: [:registrations]
+
+  devise_scope :user do
+    get  "/users/sign_up", to: "devise/registrations#new",     as: :new_user_registration
+    post "/users",         to: "devise/registrations#create",  as: :user_registration
+  end
+
   root to: 'films#index'
 end
